0f4839a61a4d2b77629efcd0725eb1db8c7eb3fe
Odunayo <odunayo.adekoya@yahoo.com>
2016-02-16 14:16:10 +0100

    4th Commit

--- string_loop.rb
+++ string_loop.rb
@@ -2,7 +2,8 @@ words = "Ikechukwuyeremefuna"
 words.upcase!
 each_word = words.split""
 i = 1
-each_word.each do |s| 
+while words.length >= i
+  each_word.each {|s| 
   puts "#{i}. #{s}"
-  i += 1
+  i += 1}
 end
